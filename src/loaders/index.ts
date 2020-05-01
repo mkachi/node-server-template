e9603a7930a4a2b0f2d143299604b1e105a444ab
MKachi <mkachi@naver.com>
2020-05-01 22:26:23 +0900

    winston에서 morgan의 로그 출력

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -2,6 +2,7 @@ import dependencyLoader from './dependency'
 import moduleLoader from './module'
 import express, { Router } from 'express'
 import cors from 'cors'
+import morgan from 'morgan'
 import logger from '../utils/logger'
 
 export default async (server: express.Express) => {
@@ -11,6 +12,13 @@ export default async (server: express.Express) => {
     const router = Router()
     await moduleLoader(router)
 
+    const morganStream = {
+      write: (text: string) => {
+        logger.route(text.replace('\n', ''))
+      },
+    }
+
+    server.use(morgan('combined', { stream: morganStream }))
     server.use(router)
     server.use(cors())
 
